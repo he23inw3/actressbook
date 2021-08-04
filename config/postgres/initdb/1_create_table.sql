904a0eab2d77dfa613f408d90f13bcc250df5fc9
he23inw3 <ma7k5px@gmail.com>
2021-08-04 22:50:34 +0900

    DDL修正

--- config/postgres/initdb/1_create_table.sql
+++ config/postgres/initdb/1_create_table.sql
@@ -1,5 +1,5 @@
 ﻿-- Project Name : noname
--- Date/Time    : 2021/07/28 22:02:15
+-- Date/Time    : 2021/08/04 22:47:43
 -- Author       : ma7k5
 -- RDBMS Type   : PostgreSQL
 -- Application  : A5:SQL Mk-2
@@ -10,18 +10,28 @@
   この機能は一時的に $$TableName のような一時テーブルを作成します。
 */
 
--- 管理テーブル
+-- お気に入りテーブル
 --* BackupToTempTable
-drop table if exists TBL_MANAGEMENT cascade;
+drop table if exists TBL_FAVORITE cascade;
 
 --* RestoreFromTempTable
-create table TBL_MANAGEMENT (
-  MANAGEMENT_ID serial not null
-  , AGENT_ID varchar not null
-  , MANAGEMET_STATUS character default 1 not null
-  , CREATED_DATE date
-  , UPDATED_DATE date
-  , constraint TBL_MANAGEMENT_PKC primary key (MANAGEMENT_ID)
+create table TBL_FAVORITE (
+  USER_ID integer not null
+  , ACTRESS_ID integer not null
+  , CREATED_DATE date default CURRENT_DATE
+  , constraint TBL_FAVORITE_PKC primary key (USER_ID,ACTRESS_ID)
+) ;
+
+-- ログイン履歴
+--* BackupToTempTable
+drop table if exists TBL_LOGIN_HISTORY cascade;
+
+--* RestoreFromTempTable
+create table TBL_LOGIN_HISTORY (
+  LOGIN_HISTORY_ID serial not null
+  , USER_ID integer not null
+  , LOGINED_DATETIME datetime default CURRENT_TIMESTAMP not null
+  , constraint TBL_LOGIN_HISTORY_PKC primary key (LOGIN_HISTORY_ID)
 ) ;
 
 -- ナイステーブル
@@ -32,22 +42,10 @@ drop table if exists TBL_NICE cascade;
 create table TBL_NICE (
   ACTRESS_ID integer not null
   , USER_ID integer not null
-  , POINT integer not null
+  , AVAILABLE_FLAG char default 1 not null
   , CREATED_DATE date
   , UPDATED_DATE date
-) ;
-
--- ナイステーブルWK
---* BackupToTempTable
-drop table if exists TBL_NICE_WK cascade;
-
---* RestoreFromTempTable
-create table TBL_NICE_WK (
-  ACTRESS_ID integer not null
-  , USER_ID integer not null
-  , POINT integer not null
-  , CREATED_DATE date
-  , UPDATED_DATE date
+  , constraint TBL_NICE_PKC primary key (ACTRESS_ID,USER_ID)
 ) ;
 
 -- 利用者テーブル
@@ -60,7 +58,7 @@ create table TBL_USER (
   , NAME varchar not null
   , MAIL_ADDRESS varchar
   , PASSWORD varchar not null
-  , ACTRESS_ID varchar not null
+  , ROLE char default 1 not null
   , DELETE_FLG char default 0 not null
   , CREATED_DATE date default CURRENT_DATE
   , UPDATED_DATE date default CURRENT_DATE
@@ -91,50 +89,29 @@ create table TBL_ACTRESS (
 create unique index ACTRESS_INDEX1
   on TBL_ACTRESS(NAME);
 
--- エージェントテーブル
---* BackupToTempTable
-drop table if exists TBL_AGENT cascade;
+comment on table TBL_FAVORITE is 'お気に入りテーブル';
+comment on column TBL_FAVORITE.USER_ID is '利用者ID';
+comment on column TBL_FAVORITE.ACTRESS_ID is '女優ID';
+comment on column TBL_FAVORITE.CREATED_DATE is '作成日付';
 
---* RestoreFromTempTable
-create table TBL_AGENT (
-  AGENT_ID varchar not null
-  , AGENT_NAME varchar not null
-  , ROLE char default 1 not null
-  , CREATED_DATE date not null
-  , UPDATED_DATE date
-  , constraint TBL_AGENT_PKC primary key (AGENT_ID)
-) ;
-
-create unique index TBL_AGENT_IX1
-  on TBL_AGENT(AGENT_NAME);
-
-comment on table TBL_MANAGEMENT is '管理テーブル:アプリ管理情報を格納';
-comment on column TBL_MANAGEMENT.MANAGEMENT_ID is '管理ID';
-comment on column TBL_MANAGEMENT.AGENT_ID is 'エージェントID';
-comment on column TBL_MANAGEMENT.MANAGEMET_STATUS is '管理ステータス:1: 通常、2: メンテナンス';
-comment on column TBL_MANAGEMENT.CREATED_DATE is '作成日付';
-comment on column TBL_MANAGEMENT.UPDATED_DATE is '更新日付';
+comment on table TBL_LOGIN_HISTORY is 'ログイン履歴';
+comment on column TBL_LOGIN_HISTORY.LOGIN_HISTORY_ID is 'ログイン履歴ID';
+comment on column TBL_LOGIN_HISTORY.USER_ID is '利用者ID';
+comment on column TBL_LOGIN_HISTORY.LOGINED_DATETIME is 'ログイン日時:ログインした日時';
 
 comment on table TBL_NICE is 'ナイステーブル:前日までのナイスを格納';
 comment on column TBL_NICE.ACTRESS_ID is '女優ID';
 comment on column TBL_NICE.USER_ID is '利用者ID';
-comment on column TBL_NICE.POINT is 'ポイント';
+comment on column TBL_NICE.AVAILABLE_FLAG is '有効フラグ:1: 有効 0: 無効';
 comment on column TBL_NICE.CREATED_DATE is '作成日付';
 comment on column TBL_NICE.UPDATED_DATE is '更新日付';
-
-comment on table TBL_NICE_WK is 'ナイステーブルWK:当日までのナイスを格納';
-comment on column TBL_NICE_WK.ACTRESS_ID is '女優ID';
-comment on column TBL_NICE_WK.USER_ID is '利用者ID';
-comment on column TBL_NICE_WK.POINT is 'ポイント';
-comment on column TBL_NICE_WK.CREATED_DATE is '作成日付';
-comment on column TBL_NICE_WK.UPDATED_DATE is '更新日付';
 
 comment on table TBL_USER is '利用者テーブル:利用者情報を格納';
 comment on column TBL_USER.USER_ID is '利用者ID:利用者ID';
 comment on column TBL_USER.NAME is '利用者名:利用者名';
 comment on column TBL_USER.MAIL_ADDRESS is 'メールアドレス:メールアドレス';
 comment on column TBL_USER.PASSWORD is 'パスワード:パスワード';
-comment on column TBL_USER.ACTRESS_ID is '女優ID:好きな女優のID';
+comment on column TBL_USER.ROLE is '役割:1: 一般 2: 管理者';
 comment on column TBL_USER.DELETE_FLG is '削除フラグ:削除フラグ 1: TRUE 0: FALSE';
 comment on column TBL_USER.CREATED_DATE is '作成日付:作成日付';
 comment on column TBL_USER.UPDATED_DATE is '更新日付:更新日付';
@@ -152,10 +129,3 @@ comment on column TBL_ACTRESS.INFO_URL is '詳細情報URL';
 comment on column TBL_ACTRESS.BIRTHDAY is '生年月日';
 comment on column TBL_ACTRESS.CREATED_DATE is '作成日付';
 comment on column TBL_ACTRESS.UPDATED_DATE is '更新日付';
-
-comment on table TBL_AGENT is 'エージェントテーブル:アプリ管理者情報を格納';
-comment on column TBL_AGENT.AGENT_ID is 'エージェントID:エージェントID';
-comment on column TBL_AGENT.AGENT_NAME is 'エージェント名:エージェント名';
-comment on column TBL_AGENT.ROLE is '役割:1:  一般、2: 管理者';
-comment on column TBL_AGENT.CREATED_DATE is '作成日付:作成日付';
-comment on column TBL_AGENT.UPDATED_DATE is '更新日付:更新日付';
